69e0f158aa94341a3b439cd92270d0b38f5acd78
Raymond Li <rli@raymonds-mbp.attlocal.net>
2016-08-17 21:30:37 -0700

    Initial Commit

---
+++ JB/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JB
+//
+//  Created by Raymond Li on 8/17/16.
+//  Copyright © 2016 Raymond Li. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
